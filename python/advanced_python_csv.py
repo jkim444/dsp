8c55c831304946268ef60537d9165a1b4474abaa
James Kim <W@Jamess-MacBook-Pro.local>
2017-06-16 02:07:48 -0700

    code done

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,14 @@
-PLACE YOUR CODE HERE
+import csv
+import advanced_python_regex
+
+def main():
+	degrees, titles, addresses, domains = advanced_python_regex.readCsv("faculty.csv")
+	
+	writeFile = open("emails.csv", 'w')
+	for email in addresses:
+		writeFile.write(email + "\n")
+
+	writeFile.close()
+
+if __name__ == "__main__":
+	main()
